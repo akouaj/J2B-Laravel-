c4e19707ceaaccad5b73d398616f34bd7849cd6c
akouaj <100274003+akouaj@users.noreply.github.com>
2022-02-23 09:52:26 +0100

    Ajout du projet Laravel

---
+++ J2B/app/Http/Controllers/EditionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+Use Illuminate\Http\Request;
+
+class EditionController extends Controllers
+
+{
+    public function show()
+    {
+        return view('Facture');
+    }
+
+
+}
+
